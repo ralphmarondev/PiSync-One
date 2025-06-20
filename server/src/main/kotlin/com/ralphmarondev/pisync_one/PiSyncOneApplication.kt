dae87564fcc2c58123420250c975d23e7bb5a312
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-20 21:42:33 +0800

    server: initial commit for server

---
+++ server/src/main/kotlin/com/ralphmarondev/pisync_one/PiSyncOneApplication.kt
@@ -0,0 +1,11 @@
+package com.ralphmarondev.pisync_one
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PiSyncOneApplication
+
+fun main(args: Array<String>) {
+	runApplication<PiSyncOneApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/ralphmarondev/pisync_one/PisyncOneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ralphmarondev.pisync_one
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PisyncOneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
